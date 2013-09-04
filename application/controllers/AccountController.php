fcd6b435bf9567bde9af28dd0be4ce96abf1d535
mohsinabbas <mohsinpucit@gmail.com>
2013-09-04 18:47:32 +0500

    page 81

--- application/controllers/AccountController.php
+++ application/controllers/AccountController.php
@@ -15,21 +15,37 @@ class AccountController extends Zend_Controller_Action
 
     public function successAction()
     {
-		$email = $this->_request->getParam('email');
-		$username = $this->_request->getParam('username');
-		$password = $this->_request->getParam('password');
-		
-		//Save the user into the system.        
+	//Check if the submitted data is POST type
+        if($this->_request->isPost()){
+        $email = $this->_request->getPost("email");
+        $username = $this->_request->getPost("username");
+        $password = $this->_request->getPost("password");
+        //Save the user into the system.
+        }else{
+        throw new Exception("Whoops. Wrong way of submitting your information.");
+        }       
     }
 
     public function newAction()
     {
         // action body
     }
+    
+    /**
+    * Activate Account. Used once the user
+    * receives a welcome email and decides to authenticate
+    * their account.
+    *
+    */
 
     public function activateAction()
     {
-        // action body
+        //Fetch the email to update from the query param 'email'
+        $emailToActivate = $this->_request->getQuery("email");
+        //Check if the email exists
+        //Activate the Account.
+        echo $emailToActivate;
+        
     }
 
 
