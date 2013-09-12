27b9303766f3c50a6302904f69354a37b77e5deb
mohsinabbas <mohsinpucit@gmail.com>
2013-09-12 18:18:06 +0500

    working on controller account and artist on page 95

--- application/controllers/ArtistController.php
+++ application/controllers/ArtistController.php
@@ -28,8 +28,43 @@ class ArtistController extends Zend_Controller_Action
         // action body
     }
 
+    public function newAction()
+    {
+        //Get all the genres
+		$genres = array("Electronic",
+		"Country",
+		"Rock",
+		"R & B",
+		"Hip-Hop",
+		"Heavy-Metal",
+		"Alternative Rock",
+		"Christian",
+		"Jazz",
+		"Pop");
+		//Set the view variables
+		$this->view->genres = $genres;		
+    }
+
+    public function saveArtistAction()
+    {
+        //get Posted data 
+		$artistName = $this->_request->getPost('artistName');
+		$genre = $this->_request->getPost('genre');
+		$isFav = $this->_request->getPost('isFav');
+		$rating = $this->_request->getPost('rating');
+		
+		
+		//After validation save data in DB
+				
+		
+    }
+
 
 }
+
+
+
+
 
 
 
